cd257ed12ee9e0f90dd820ee361594e3e6646337
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2013-09-28 22:05:15 -0400

    Create DatePrototypes.js

---
+++ DatePrototypes.js
@@ -0,0 +1,33 @@
+//Properties
+Date.dayNames = 'Sunday|Monday|Tuesday|Wednesday|Thursday|Friday|Saturday'.split('|');
+Date.monthNames = 'January|February|March|April|May|June|July|August|September|October|November|December'.split('|');
+
+
+
+//Methods
+Date.prototype.clearTime = function(){
+	return this.setHours(0).setMinutes(0).setSeconds(0).setMilliseconds(0);
+};
+
+Date.prototype.getDayName = function(){ 
+	return Date.dayNames[this.getDay()];
+};
+
+Date.prototype.getMonthName = function(){
+	return Date.monthNames[this.getMonth()];
+};
+
+
+Date.prototype.isAfter = function(otherDate){
+	if(otherDate == null){
+	  return false; 
+	}
+	return (this.getTime() > otherDate.getTime());
+};
+
+Date.prototype.isBefore = function(otherDate){
+	if(otherDate == null){
+		return false;
+	}
+	return (this.getTime() < otherDate.getTime());
+};
