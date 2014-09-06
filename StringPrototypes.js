b2e75c0d628df633a98b355d3ffd5ef86d8d3077
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2014-09-06 13:56:57 -0400

    Added tool to convert string to an XML DOM

--- StringPrototypes.js
+++ StringPrototypes.js
@@ -1,3 +1,15 @@
+String.prototype.toXMLDOM = function(){
+	var xml;
+	if(window.DOMParser){
+		xml = (new DOMParser).parseFromString(this, 'text/xml');
+	} else if(window.ActiveXObject){
+		xml = [new ActiveXObject('Microsoft.XMLDOM'), this];
+		xml[0].async = false;
+		xml[0].loadXML(xml[1]);
+		xml = xml[0];
+	}
+	return xml;
+};
 String.prototype.trim = function(){
 	return this.replace(/^\s*/, "").replace(/\s*$/, "");
 }
