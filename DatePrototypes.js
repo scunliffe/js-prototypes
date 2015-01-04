257d3fc6072938c7da2e71010637ce84ea4e8e4c
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2015-01-03 19:06:09 -0500

    Added toISOString implementation for IE8 and below

--- DatePrototypes.js
+++ DatePrototypes.js
@@ -98,7 +98,24 @@ Date.prototype.now = function(){
 	return new Date();
 };
 
-
 Date.prototype.today = function(){
 	return new Date().clearTime();
 };
+
+//For IE8 and below only
+if(!Date.prototype.toISOString){
+	(function(){
+		function pad(number){
+			if(number < 10){
+				return '0' + number;
+			}
+			return number;
+		}
+		Date.prototype.toISOString = function(){
+			return this.getUTCFullYear() + '-' + pad(this.getUTCMonth() + 1) + '-' + pad(this.getUTCDate()) +
+				'T' + pad(this.getUTCHours()) + ':' + pad(this.getUTCMinutes()) + ':' + pad(this.getUTCSeconds()) +
+				'.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+				'Z';
+		};
+	}());
+}
