bdbad47cbb7a787b9a003f160b1be5f0f71e169e
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2015-03-20 10:47:13 -0400

    added toDegrees and toRadians to use constants

--- NumberPrototypes.js
+++ NumberPrototypes.js
@@ -1,3 +1,9 @@
+//Properties
+Number.prototype.PI_OVER_ONE_EIGHTY = (Math.PI / 180);
+Number.prototype.ONE_EIGHTY_OVER_PI = (180 / Math.PI);
+
+
+//Methods
 Number.prototype.formatPretty = function(decimalPlaces, d, thousandsDelimiter){
 	var n = this, decimalPlaces = isNaN(decimalPlaces = Math.abs(decimalPlaces)) ? 2 : decimalPlaces, d = d == undefined ? "," : d, thousandsDelimiter = thousandsDelimiter == undefined ? "." : thousandsDelimiter, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(decimalPlaces)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
 	return s + (j ? i.substr(0, j) + thousandsDelimiter : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousandsDelimiter) + (decimalPlaces ? d + Math.abs(n - i).toFixed(decimalPlaces).slice(2) : "");
@@ -14,9 +20,9 @@ Number.prototype.pad = function(padToLength, character){
 };
 
 Number.prototype.toDegrees = function(){
-	return this * (180 / Math.PI);
+	return this * this.ONE_EIGHTY_OVER_PI;
 };
 
 Number.prototype.toRadians = function(){
-	return this * (Math.PI / 180);
+	return this * this.PI_OVER_ONE_EIGHTY;
 };
