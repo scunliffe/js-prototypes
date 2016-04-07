06e6d4d3e65f19f6376798f25ca066e6e10ae8c5
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2016-04-07 14:15:58 -0400

    Added getDistance method

--- MathPrototypes.js
+++ MathPrototypes.js
@@ -26,6 +26,10 @@ MathUtils.createRange = function(start, stop){
 	return ret;
 };
 
+MathUtils.getDistance = function(p1, p2){
+	return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+};
+
 MathUtils.getGreatestCommonDivisor = function(a, b){
 	return b ? Math.getGreatestCommonDivisor(b, a % b) : a;
 };
