a7990adab1b219176d2ba6b8e8830663f1d5b7d4
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2020-08-31 12:03:35 -0400

    Corrected default decimal and thousands separator characters (they were reversed)

--- NumberPrototypes.js
+++ NumberPrototypes.js
@@ -5,7 +5,7 @@ Number.prototype.ONE_EIGHTY_OVER_PI = (180 / Math.PI);
 
 //Methods
 Number.prototype.formatPretty = function(decimalPlaces, d, thousandsDelimiter){
-	var n = this, decimalPlaces = isNaN(decimalPlaces = Math.abs(decimalPlaces)) ? 2 : decimalPlaces, d = d == undefined ? "," : d, thousandsDelimiter = thousandsDelimiter == undefined ? "." : thousandsDelimiter, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(decimalPlaces)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
+	var n = this, decimalPlaces = isNaN(decimalPlaces = Math.abs(decimalPlaces)) ? 2 : decimalPlaces, d = d == undefined ? "." : d, thousandsDelimiter = thousandsDelimiter == undefined ? "," : thousandsDelimiter, s = n < 0 ? "-" : "", i = parseInt(n = Math.abs(+n || 0).toFixed(decimalPlaces)) + "", j = (j = i.length) > 3 ? j % 3 : 0;
 	return s + (j ? i.substr(0, j) + thousandsDelimiter : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousandsDelimiter) + (decimalPlaces ? d + Math.abs(n - i).toFixed(decimalPlaces).slice(2) : "");
 };
 
